5f04126e1781ae7648e7d3b4818d0b2c544c1f2e
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-06-05 22:01:00 +0100

    added java script files

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-let count = 0
 
-
-console.log(count)
-
-
+// console.log(count)
 
 // document.getEleme ntsById("count").innerText = 5
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
 
+//  initialize the count as 0
+let count = 0
+
+function increment() {
+    count = count + 1
+    document.getElementById("count-el").innerHTML = count
+    console.log(count)
+}
+//  listen for clicks on the increment button
+//  increment the count variable when the button is clicked 
+//  change the count-el in the HTML 
+
+ 
