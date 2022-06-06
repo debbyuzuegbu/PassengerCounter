d07903b1c6adc3dc73aba61a68285f44cac53688
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-06-06 01:02:20 +0100

    added javascript file for save button

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 
 // console.log(count)
 
-// document.getEleme ntsById("count").innerText = 5
-
-//  initialize the count as 0
 let count = 0
-
 function increment() {
     count = count + 1
     document.getElementById("count-el").innerHTML = count
     console.log(count)
 }
-//  listen for clicks on the increment button
-//  increment the count variable when the button is clicked 
-//  change the count-el in the HTML 
 
- 
+function save() {
+    console.log(count)
+} 
+save()
